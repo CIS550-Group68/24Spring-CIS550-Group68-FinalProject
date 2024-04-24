bf98535eaa3d3ab29f1eaed94c359814fc36f726
Haoze Wu <haozewu@seas.upenn.edu>
2024-04-23 20:53:19 -0400

    import axios. Finish frontend API based on OpenAPI doc

---
+++ frontend/public/api/AxiosHelper.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+function getInstance() {
+    return axios.create({
+        baseURL: "/api",
+        // timeout: 100000 | timeout,
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+}
+
+const axiosInstance = getInstance();
+
+const axiosGet = async (url, param ) => {
+    const resp = await axiosInstance.get(url, { params: param });
+    return resp;
+};
+
+const axiosPost = async (url, param, data) => {
+    const resp = await axiosInstance.post(url, data, { params: param });
+    return resp;
+}
+
+const axiosPut = async (url, param, data) => {
+    const resp = await axiosInstance.put(url, data, { params: param });
+    return resp;
+}
+
+const axiosDelete = async (url, param) => {
+    const resp = await axiosInstance.delete(url, { params: param });
+    return resp;
+}
+
+const axiosPatch = async (url, param, data) => {
+    const resp = await axiosInstance.patch(url, data, { params: param });
+    return resp;
+}
+
+export { axiosInstance, axiosGet, axiosPost, axiosPut, axiosDelete, axiosPatch };
---
+++ frontend/public/api/PaperAPI.js
@@ -0,0 +1,115 @@
+import {
+    axiosInstance, axiosGet, axiosPost, axiosPut, axiosDelete, axiosPatch,
+} from "./AxiosHelper";
+
+async function getAuthorById(authorId) {
+    return await axiosGet(`/author/${authorId}`);
+}
+
+async function getAuthorByName(authorName) {
+    return await axiosGet(`/author`, { authorName });
+}
+
+async function getNameAndAliasByAuthorId(authorId) {
+    return await axiosGet(`/authorNames/${authorId}`);
+}
+
+async function getTopAuthots(topN, criterion) {
+    return await axiosGet(`/topAuthors`, { topN, criterion });
+}
+
+async function getAuthorFields(authorId) {
+    return await axiosGet(`/authorFields/${authorId}`);
+}
+
+async function getAuthorCollaborators(authorId) {
+    return await axiosGet(`/authorCollaborators/${authorId}`);
+}
+
+async function getPaperbyId(paperId) {
+    return await axiosGet(`/paper/${paperId}`);
+}
+
+async function getPaperbyTitle(title) {
+    return await axiosGet(`/papers`, { title });
+}
+
+async function getPaperFields(paperId) {
+    return await axiosGet(`/paperFields/${paperId}`);
+}
+
+async function getPaperAuthors(paperId) {
+    return await axiosGet(`/paperAuthors/${paperId}`);
+}
+
+async function getPaperExternalIds(paperId) {
+    return await axiosGet(`/paperExternalIds/${paperId}`);
+}
+
+async function getTopPapers(topN, fieldId) {
+    return await axiosGet(`/topPapers`, { topN, field_id: fieldId });
+}
+
+async function getAverageTopPapers(topN, fieldId) {
+    return await axiosGet(`/averageTopPapers`, { topN, field_id: fieldId });
+}
+
+async function getPaperbyJournal(journal) {
+    return await axiosGet(`/journalPaper`, { journal })
+}
+
+async function getPaperByVenue(venue) {
+    return await axiosGet(`/venuePaper`, { venue })
+}
+
+async function getTopJournal(topN, criterion) {
+    return await axiosGet(`/topJournal`, { topN,  criterion });
+}
+
+async function getTopVenue(topN, criterion) {
+    return await axiosGet(`/topVenue`, { topN, criterion });
+}
+
+async function getFieldById(fieldId) {
+    return await axiosGet(`/field/${fieldId}`);
+}
+
+async function getFieldByName(fieldName) {
+    return await axiosGet(`/field`, { fieldName });
+}
+
+async function getPaperOfField(fieldId) {
+    return await axiosGet(`/fieldPapers/${fieldId}`);
+}
+
+async function getAuthorOfField(fieldId) {
+    return await axiosGet(`/fieldAuthors/${fieldId}`);
+}
+
+async function getTopAuthorOfField(topN, fieldId, criterion) {
+    return await axiosGet(`/fieldTopAuthors`, { topN, field_id: fieldId, criterion });
+}
+
+async function getTopPaperOfField(topN, fieldId, criterion) {
+    return await axiosGet(`/fieldTopPapers`, { topN, field_id: fieldId, criterion });
+}
+
+async function getTopJornalOfField(topN, fieldId, criterion) {
+    return await axiosGet(`/fieldTopJournals`, { topN, field_id: fieldId, criterion });
+}
+
+async function getTopVenueOfField(topN, fieldId, criterion) {
+    return await axiosGet(`/fieldTopVenues`, { topN, field_id: fieldId, criterion });
+}
+
+async function getAverageCitationCountOfField(fieldId) {
+    return await axiosGet(`/fieldAverageCitationCount/${fieldId}`);
+}
+
+export {
+    getAuthorById, getAuthorByName, getNameAndAliasByAuthorId, getTopAuthots, getAuthorFields, getAuthorCollaborators,
+    getPaperbyId, getPaperby, getPaperFields, getPaperAuthors, getPaperExternalIds, getTopPapers, getAverageTopPapers,
+    getPaperbyJournal, getPaperByVenue, getTopJournal, getTopVenue, getFieldById, getFieldByName, getPaperOfField,
+    getAuthorOfField, getTopAuthorOfField, getTopPaperOfField, getTopJornalOfField, getTopVenueOfField, 
+    getAverageCitationCountOfField,
+}
